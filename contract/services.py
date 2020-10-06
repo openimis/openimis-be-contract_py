a7c1a2ba7a59a8df237f881bc805f0dd12b63d1f
Damian Borowiecki <dborowiecki@soldevelo.com>
2020-10-06 09:18:42 +0200

    OFS-14: Added contract models and empty services

---
+++ contract/services.py
@@ -0,0 +1,48 @@
+import xml.etree.ElementTree as ET
+from django.core.exceptions import PermissionDenied
+import core
+from django.db import connection, transaction
+
+from django.conf import settings
+
+
+@core.comparable
+class PolicyHolderContract(object):
+
+    def __init__(self, policy_holder):
+        self.policy_holder = policy_holder
+        pass
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
+    def submit(self, submit):
+        pass
+
+    def amend(self, submit):
+        pass
+
+
+@core.comparable
+class PolicyHolderContractDetails(object):
+
+    def __init__(self, policy_holder):
+        self.policy_holder = policy_holder
+        pass
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
+
+@core.comparable
+class Payment(object):
+
+    def __init__(self, policy_holder):
+        self.policy_holder = policy_holder
+        pass
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
+    def submit(self, payment):
+        pass
