a30e4c9d5192d1a59d1f0c10d2335d5bd0557f28
sniedzielski <sniedzielski@soldevelo.com>
2020-12-21 15:57:51 +0100

    OFS-61: revert mutations

--- contract/gql_mutations/__init__.py
+++
@@ -1,6 +0,0 @@
-from .input_types import *
-from .exceptions import *
-from .create_contract import CreateContractMutation
-from .update_contract import UpdateContractMutation
-from .delete_contract import DeleteContractMutation
-from .submit_contract import SubmitContractMutation
