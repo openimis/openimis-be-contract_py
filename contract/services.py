7d04825eb2361d9bcb629d0a2502f52ca1971de9
lruzicki <lukasz.ruz@gmail.com>
2023-03-17 10:17:35 +0100

    OTC-963: deleted print statement

--- contract/services.py
+++ contract/services.py
@@ -953,6 +953,5 @@ def _send_email_notify_counter(code, name, contact_name, email):
 
 def check_unique_code(code):
     if ContractModel.objects.filter(code=code, is_deleted=False).exists():
-        print(ContractModel.objects.filter(code=code, is_deleted__isnull=False).first())
         return [{"message": "Contract code %s already exists" % code}]
     return []
